0cee689bc1d69fcfd551b53f6d179751e3a19690
Mahesh Maney R <mahesh-maney@users.noreply.github.com>
2023-08-11 15:12:15 +0530

    Initial commit

---
+++ src/main/java/com/tarento/upsmf/examsAndAdmissions/ExamsAndAdmissionsApplication.java
@@ -0,0 +1,13 @@
+package com.tarento.upsmf.examsAndAdmissions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsAndAdmissionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsAndAdmissionsApplication.class, args);
+	}
+
+}
