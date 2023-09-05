ffad14d5b7cf23338205766015a22796f56bc9df
Radhesh.B.H <44744695+Radheshhathwar@users.noreply.github.com>
2023-09-05 12:07:16 +0530

    Update NotificationConsumer.java

--- src/main/java/com/tarento/upsmf/examsAndAdmissions/consumer/NotificationConsumer.java
+++ src/main/java/com/tarento/upsmf/examsAndAdmissions/consumer/NotificationConsumer.java
@@ -3,34 +3,32 @@ package com.tarento.upsmf.examsAndAdmissions.consumer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tarento.upsmf.examsAndAdmissions.model.InstituteList;
 import com.tarento.upsmf.examsAndAdmissions.service.impl.NotificationServiceImpl;
+import lombok.extern.slf4j.Slf4j; // Import Lombok's @Slf4j annotation
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-
+@Slf4j
 @Service
 public class NotificationConsumer {
-	Logger logger = LogManager.getLogger(NotificationConsumer.class);
 
-	@Autowired
-	private ObjectMapper mapper;
+    @Autowired
+    private ObjectMapper mapper;
 
-	@Autowired
-	private NotificationServiceImpl notificationService;
+    @Autowired
+    private NotificationServiceImpl notificationService;
 
-	@KafkaListener( groupId = "workflowNotificationTopic-consumer", topics = "${kafka.topics.workflow.notification}")
-	public void processMessage(ConsumerRecord<String, String> data) {
-		InstituteList wfRequest = null;
-		try {
-			String message = String.valueOf(data.value());
-			wfRequest = mapper.readValue(message, InstituteList.class);
-			logger.info("Received data in notification consumer : {}", mapper.writeValueAsString(wfRequest));
-			notificationService.sendEmailNotification(wfRequest);
-		} catch (Exception ex) {
-			logger.error("Error while deserialization the object value", ex);
-		}
-	}
+    @KafkaListener(groupId = "workflowNotificationTopic-consumer", topics = "${kafka.topics.workflow.notification}")
+    public void processMessage(ConsumerRecord<String, String> data) {
+        InstituteList wfRequest = null;
+        try {
+            String message = String.valueOf(data.value());
+            wfRequest = mapper.readValue(message, InstituteList.class);
+            log.info("Received data in notification consumer : {}", mapper.writeValueAsString(wfRequest));
+            notificationService.sendEmailNotification(wfRequest);
+        } catch (Exception ex) {
+            log.error("Error while deserialization the object value", ex);
+        }
+    }
 }
