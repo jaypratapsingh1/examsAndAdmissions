6827e447aebb316f86a64058974176d0a6b3df2f
jay pratap singh <jaypratap.singh@tarento.com>
2023-09-07 13:17:23 +0530

    added validation code and status code changes

--- src/main/java/com/tarento/upsmf/examsAndAdmissions/controller/HallTicketController.java
+++ src/main/java/com/tarento/upsmf/examsAndAdmissions/controller/HallTicketController.java
@@ -5,6 +5,7 @@ import com.tarento.upsmf.examsAndAdmissions.model.dto.DataCorrectionRequestDTO;
 import com.tarento.upsmf.examsAndAdmissions.service.HallTicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,13 +21,26 @@ public class HallTicketController {
 
     @GetMapping("/download")
     public ResponseEntity<ByteArrayResource> getHallTicket(@RequestParam String examRegistrationNumber, @RequestParam String dateOfBirth) {
+        if (examRegistrationNumber == null || dateOfBirth == null || examRegistrationNumber.isEmpty() || dateOfBirth.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
         byte[] data = hallTicketService.getHallTicket(examRegistrationNumber, dateOfBirth).getBody();
+
+        if (data == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
         ByteArrayResource resource = new ByteArrayResource(data);
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
     }
 
     @PostMapping("/dataCorrection/request")
     public ResponseEntity<String> requestDataCorrection(@RequestBody DataCorrectionRequestDTO request) {
+        if (request == null || request.getStudentId() == null || request.getCorrectionDetails() == null || request.getCorrectionDetails().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request data");
+        }
+
         hallTicketService.requestHallTicketDataCorrection(request.getStudentId(), request.getCorrectionDetails());
         return ResponseEntity.ok("Data correction requested");
     }
@@ -38,6 +52,10 @@ public class HallTicketController {
 
     @PostMapping("/dataCorrection/{requestId}/approve")
     public ResponseEntity<String> approveDataCorrection(@PathVariable Long requestId) {
+        if (requestId == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request ID");
+        }
+
         hallTicketService.approveDataCorrection(requestId);
         return ResponseEntity.ok("Request approved");
     }
@@ -46,6 +64,10 @@ public class HallTicketController {
     public ResponseEntity<String> rejectDataCorrection(
             @PathVariable Long requestId,
             @RequestParam String rejectionReason) {
+        if (requestId == null || rejectionReason == null || rejectionReason.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid input data");
+        }
+
         hallTicketService.rejectDataCorrection(requestId, rejectionReason);
         return ResponseEntity.ok("Request rejected");
     }
