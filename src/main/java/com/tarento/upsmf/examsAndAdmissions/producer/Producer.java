95be03a7c3b48fea3aefce9c99828a1b09563702
Shishir Suman <74050813+shishirsuman092@users.noreply.github.com>
2023-09-08 12:55:53 +0530

    Update Producer.java
    commenting producer code

--- src/main/java/com/tarento/upsmf/examsAndAdmissions/producer/Producer.java
+++ src/main/java/com/tarento/upsmf/examsAndAdmissions/producer/Producer.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class Producer {
 
-    @Autowired
-    KafkaTemplate<String, Object> kafkaTemplate;
+    //@Autowired
+    //KafkaTemplate<String, Object> kafkaTemplate;
 
     public void push(String topic, Object value) {
         kafkaTemplate.send(topic, value);
