4115b677fc17adc6a9673662ceac4b17757e1f20
Ankit Verma <ankitv89@gmail.com>
2023-09-22 08:36:14 +0530

    Update InstituteCourseMappingController.java

--- src/main/java/com/tarento/upsmf/examsAndAdmissions/controller/InstituteCourseMappingController.java
+++ src/main/java/com/tarento/upsmf/examsAndAdmissions/controller/InstituteCourseMappingController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/exam/v1/instituteCourseMap/all")
+@RequestMapping("/api/exam/v1/instituteCourseMap")
 public class InstituteCourseMappingController {
 
     @Autowired
