3b5f7f0a0ab59c239d2b07cf246e7d9a4bdcb5df
Ankit Verma <ankitv89@gmail.com>
2023-09-22 08:30:52 +0530

    kong gateway experiment

--- src/main/java/com/tarento/upsmf/examsAndAdmissions/controller/InstituteCourseMappingController.java
+++ src/main/java/com/tarento/upsmf/examsAndAdmissions/controller/InstituteCourseMappingController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/instituteCourseMap")
+@RequestMapping("/api/exam/v1/instituteCourseMap/all")
 public class InstituteCourseMappingController {
 
     @Autowired
