e12531a299834747df8c24eb90dfe72079b9e783
jay pratap singh <jaypratap.singh@tarento.com>
2023-10-05 12:47:17 +0530

    changed responseDTO

--- src/main/java/com/tarento/upsmf/examsAndAdmissions/model/ResponseDto.java
+++ src/main/java/com/tarento/upsmf/examsAndAdmissions/model/ResponseDto.java
@@ -7,23 +7,54 @@ import java.util.Map;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-
 public class ResponseDto {
 
     private String id;
     private String ver;
     @Getter
-	private String ts;
+    private String ts;
     private ResponseParams params;
     private HttpStatus responseCode;
+    private int responseCodeNumeric;
+    private ErrorDetails error; // Error details
+    private transient Map<String, Object> result = new HashMap<>();
 
-    private transient Map<String, Object> response = new HashMap<>();
+    public static class ErrorDetails {
+        private String code;
+        private String message;
+        private String details;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public String getDetails() {
+            return details;
+        }
+
+        public void setDetails(String details) {
+            this.details = details;
+        }
+    }
 
     public ResponseDto() {
         this.ver = "v1";
         this.ts = String.valueOf(new Timestamp(System.currentTimeMillis()));
         this.params = new ResponseParams();
     }
+
     public ResponseDto(HttpStatus status, ResponseParams params) {
         this.ver = "v1";
         this.ts = String.valueOf(new Timestamp(System.currentTimeMillis()));
@@ -52,7 +83,7 @@ public class ResponseDto {
         this.ver = ver;
     }
 
-	public void setTs(String ts) {
+    public void setTs(String ts) {
         this.ts = ts;
     }
 
@@ -70,29 +101,46 @@ public class ResponseDto {
 
     public void setResponseCode(HttpStatus responseCode) {
         this.responseCode = responseCode;
+        this.responseCodeNumeric = responseCode.value();
+    }
+
+    public int getResponseCodeNumeric() {
+        return responseCodeNumeric;
+    }
+
+    public void setResponseCodeNumeric(int responseCodeNumeric) {
+        this.responseCodeNumeric = responseCodeNumeric;
     }
 
     public Map<String, Object> getResult() {
-        return response;
+        return result;
     }
 
     public void setResult(Map<String, Object> result) {
-        response = result;
+        this.result = result;
     }
 
     public Object get(String key) {
-        return response.get(key);
+        return result.get(key);
     }
 
     public void put(String key, Object vo) {
-        response.put(key, vo);
+        result.put(key, vo);
     }
 
     public void putAll(Map<String, Object> map) {
-        response.putAll(map);
+        result.putAll(map);
     }
 
     public boolean containsKey(String key) {
-        return response.containsKey(key);
+        return result.containsKey(key);
+    }
+
+    public ErrorDetails getError() {
+        return error;
+    }
+
+    public void setError(ErrorDetails error) {
+        this.error = error;
     }
 }
