6fc52eea683c4b13b0c01397eca1094eeabbb2cd
Shishir Suman <74050813+shishirsuman092@users.noreply.github.com>
2023-10-27 19:03:44 +0530

    updating CORS filter

--- src/main/java/com/tarento/upsmf/examsAndAdmissions/ExamsAndAdmissionsApplication.java
+++ src/main/java/com/tarento/upsmf/examsAndAdmissions/ExamsAndAdmissionsApplication.java
@@ -72,7 +72,7 @@ public class ExamsAndAdmissionsApplication {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-    	config.addAllowedOriginPattern("http://localhost:*");
+    	config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
